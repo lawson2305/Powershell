28629716831287927da28d0e95df30f58ba87e50
lawson23 <33846756+lawson2305@users.noreply.github.com>
2020-04-02 12:09:04 -0400

    Update ErrorReporting.ps1

--- ErrorReporting.ps1
+++ ErrorReporting.ps1
@@ -1,11 +1,22 @@
-#https://stackoverflow.com/questions/3404642/how-can-i-get-powershell-exception-descriptions-into-a-string
-#Gives good data like the full data from $error[0].InvocationInfo.PositionMessage
+<#
+https://stackoverflow.com/questions/3404642/how-can-i-get-powershell-exception-descriptions-into-a-string
+Gives good data like the full data from $error[0].InvocationInfo.PositionMessage
+ modified by lawson2305
+#>
 Function FriendlyErrorString ($thisError) {
     [string] $Return = $thisError.Exception
     $Return += "`r`n"
     $Return += "At line:" + $thisError.InvocationInfo.ScriptLineNumber
+    $Return += "`r`n"
     $Return += " char:" + $thisError.InvocationInfo.OffsetInLine
-    $Return += " For: " + $thisError.InvocationInfo.Line
+    $Return += "`r`n"
+    $Return += " For Line: " + $thisError.InvocationInfo.Line
+    $Return += "`r`n"
+    $Return += " Invocation: " + $thisError.InvocationInfo.mycommand
+    $Return += "`r`n"
+    $Return += " Category: " + $thisError.categoryInfo
+    $Return += "`r`n"
+    $Return += " Category: " + $thisError.categoryInfo.TargetName
     Return $Return
 }
 
